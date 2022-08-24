12c7c0c71339839c589a793eadc0b59eaa738782
Sean Oh <23ohsean@gmail.com>
2022-08-24 03:17:47 -0700

    add component structures and base particles bg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import {React,useCallback} from 'react';
 import './App.css';
+import Container from './components/Container';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
 
 function App() {
+
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+    document.querySelector("#tsparticles canvas").style.zIndex = -10;
+  }, []);
+
+  const options = {
+    background: {
+      color: {
+        value: "#0d47a1",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        directions: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 6,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Particles id='tsparticles' init={particlesInit} loaded={particlesLoaded} options={options} />
+      <Container />
     </div>
   );
 }
---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Container() {
+
+
+    return (
+        <div>
+            {/* Header */}
+            {/* Correct Page */}
+            {/* Footer */}
+            <h1>CONTAINER</h1>
+        </div>
+    )
+}
