a99e3e134136c3a61aff7292a9f4d3100522aeba
Sean Oh <23ohsean@gmail.com>
2025-03-08 00:42:48 -0800

    fixed resume dl

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,13 +3,14 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import Pdf from '../documents/Sean_Oh_Resume.pdf'
 
 function Resume() {
   return (
     <Card sx={{backgroundColor: 'white'}}>
       <CardContent>
         <Typography gutterBottom variant='h4'>Resume</Typography>
-        <Typography variant='body1'>Check out my <Link to='/Sean_Oh_Resume.pdf' target='_blank' download>resume</Link></Typography>
+        <Typography variant='body1'>Check out my <Link href={Pdf} target='_blank'>resume</Link></Typography>
       </CardContent>
     </Card>
   )
